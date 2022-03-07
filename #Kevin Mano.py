abab7cc6a1adbe9c4aac10c657b1788a1871e90e
Kevin Mano <kevin_rotijohn@yahoo.com>
2022-03-07 17:26:07 -0500

    7-March-2022: Initial Commit

---
+++ #Kevin Mano.py
@@ -0,0 +1,8 @@
+#Kevin Mano
+#7 March 2022
+#Brief: This is a project to implement a circle detector using Python and OpenCV.
+#This algorithm is from the paper titled: "CIRCLE DETECTION ON IMAGES BY LINE SEGMENT AND CIRCLE COMPLETENESS" by Truc Le and Ye Duan.
+
+import cv2
+import numpy as np
+
